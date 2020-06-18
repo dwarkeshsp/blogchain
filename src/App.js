a68958c24f50120dd43e8d3d002156eba07bee5f
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-06-18 12:19:00 -0500

    copying fireship

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LinkPreview />
     </div>
+  );
+}
+
+function LinkPreview() {
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [links, setLinks] = useState([]);
+
+  const handleSubmit = async (evt) => {
+    setLoading(true);
+    evt.preventDefault();
+    console.log(`Submitting ${text}`);
+
+    const res = await fetch(
+      "https://us-central1-blogchain-a6eac.cloudfunctions.net/scraper",
+      {
+        method: "POST",
+        body: JSON.stringify({ text }),
+      }
+    );
+
+    const data = await res.json();
+
+    setLinks(data);
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <h1>Form</h1>
+      Try this:{" "}
+      <pre>
+        some example text https://fireship.io and
+        https://fireship.io/courses/javascript/
+      </pre>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          rows="4"
+          cols="50"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+      <h2>Preview</h2>
+      <p>{text}</p>
+      {loading && <h3>Fetching link previews... 🤔🤔🤔</h3>}
+      {links.map((obj) => (
+        <PreviewCard key={obj.url} linkData={obj} />
+      ))}
+    </div>
+  );
+}
+
+function PreviewCard({ linkData }) {
+  return (
+    <a className="preview" href={linkData.url}>
+      <img src={linkData.image} />
+      <div>
+        <h4>{linkData.title}</h4>
+        <p>{linkData.description}</p>
+      </div>
+    </a>
   );
 }
 
