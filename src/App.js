d781ebec6420f8445f79dc28ff5101828fdcd391
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-06-18 19:12:02 -0500

    valid url

--- src/App.js
+++ src/App.js
@@ -10,29 +10,40 @@ function App() {
 }
 
 function LinkPreview() {
-  const [text, setText] = useState("");
+  const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
   const [links, setLinks] = useState([]);
+  const [valid, setValid] = useState(true);
 
   const handleSubmit = async (evt) => {
-    setLoading(true);
     evt.preventDefault();
-    const encoding = Buffer.from(text).toString("base64");
 
-    console.log(`Submitting ${text}`, encoding);
+    const validURL = () =>
+      url.match(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
+      );
 
-    const res = await fetch(
-      "http://localhost:5001/blogchain-a6eac/us-central1/scraper",
-      {
-        method: "POST",
-        body: JSON.stringify({ encoding }),
-      }
-    );
+    if (validURL(url)) {
+      setLoading(true);
+      const encoding = Buffer.from(url).toString("base64");
 
-    const data = await res.json();
+      console.log(`Submitting ${url}`, "encoded as", encoding);
 
-    setLinks(data);
-    setLoading(false);
+      const res = await fetch(
+        "http://localhost:5001/blogchain-a6eac/us-central1/scraper",
+        {
+          method: "POST",
+          body: JSON.stringify({ encoding }),
+        }
+      );
+
+      const data = await res.json();
+
+      setLinks(data);
+      setLoading(false);
+    } else {
+      setValid(false);
+    }
   };
 
   return (
@@ -48,14 +59,16 @@ function LinkPreview() {
           rows="4"
           cols="50"
           type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+          value={url}
+          onChange={(e) => setUrl(e.target.value.trim())}
         ></textarea>
         <br />
         <input type="submit" value="Submit" />
+        <br />
+        {!valid && "Url is not valid. Make sure to include http(s)://"}
       </form>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{url}</p>
       {loading && <h3>Fetching link previews... 🤔🤔🤔</h3>}
       {links.map((obj) => (
         <PreviewCard key={obj.url} linkData={obj} />
